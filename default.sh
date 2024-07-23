9c0fe11ce84e231c819cf795d73a95e13f3395e3
runningoec <59631013+runningoec@users.noreply.github.com>
2024-07-23 10:11:34 -0400

    Update default.sh

--- default.sh
+++ default.sh
@@ -37,6 +37,7 @@ CHECKPOINT_MODELS=(
     "https://civitai.com/api/download/models/280025"
     "https://civitai.com/api/download/models/272376?type=Model&format=SafeTensor&size=pruned&fp=fp16"
     "https://civitai.com/api/download/models/357609"
+    "https://civitai.com/api/download/models/501286?type=Model&format=SafeTensor&size=full&fp=fp16"
 )
 
 LORA_MODELS=(
